2e820f7efc2aaba975df53317bb28e3a2a0232ae
Mauricio Moraes <moraesr@poli.ufrj.br>
2020-12-13 19:36:19 -0300

    Third commit

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,3 +1,10 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
 let selected = false;
 
 const isEmpty = (value) => {
@@ -32,20 +39,6 @@ document.querySelector("#aceito").addEventListener("change", (e) => {
 })
 
 console.log(document.querySelector("#nome"))
-
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
-//
-// (function (i, s, o, g, r, a, m) {
-//   i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-//     (i[r].q = i[r].q || []).push(arguments)
-//   }, i[r].l = 1 * new Date(); a = s.createElement(o),
-//     m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-// })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 var form = document.getElementById('form_contato');
 
